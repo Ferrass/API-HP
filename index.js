6808757fba65e46a429b634edd80329b37342136
Gabriel Ferreira <gabriel.souto@clear.sale>
2022-01-11 13:48:20 -0300

    alteração da porta

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Character = require("./models/Character");
 
 const app = express();
 
-const port = process.Tenv.POR || 3000;
+const port = process.env.PORT || 3000;
 
 try {
   mongoose.connect(
