e44b50f4d6940934195ec57f397729a8da0e032e
Gabriel Ferreira <gabriel.souto@clear.sale>
2022-01-11 15:53:33 -0300

    alteração da porta

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Character = require("./models/Character");
-
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 try {
-  mongoose.connect(
-    process.env.DATABASE_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  mongoose.connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   console.log("Banco de dados concetado!");
 } catch (err) {
   console.log(`Erro ao concetar no banco de dados ${err}`);
@@ -29,7 +25,7 @@ app.get("/characters", async (req, res) => {
 
   if (characters.length === 0) {
     return res.status(404).send({ messege: "Não existe personagem cadastrados!" });
-  };
+  }
 
   res.send(characters.filter(Boolean));
 });
@@ -56,7 +52,7 @@ app.get("/character/:id", async (req, res) => {
 app.post("/character", async (req, res) => {
   const { name, species, house, actor } = req.body;
 
-  if (!name || !species || !house  || !actor) {
+  if (!name || !species || !house || !actor) {
     res.status(400).send({
       message: "Você não enviou todos os dados necessários para o cadastro",
     });
@@ -92,40 +88,40 @@ app.put("/character/:id", async (req, res) => {
 
   const { name, species, house, actor } = req.body;
 
-  if (!name || !species || !house  || !actor) {
+  if (!name || !species || !house || !actor) {
     res.status(400).send({
       messege: "Você não enviou todos os dados necessários para o atualização",
     });
     return;
   }
-  
+
   character.name = name;
   character.species = species;
   character.house = house;
-  character.actor = actor
+  character.actor = actor;
 
-  await character.save()
-  
-  res.send({messege: `Personagem atualizado com sucesso! ${character}`});
+  await character.save();
+
+  res.send({ messege: `Personagem atualizado com sucesso! ${character}` });
 });
 
 // DELETE - DELETe
-app.delete("/character/:id", async(req, res) => {
+app.delete("/character/:id", async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    res.status(400).send({messege: "Id inválido!"});
+    res.status(400).send({ messege: "Id inválido!" });
     return;
   }
 
-  const character = await Character.findById(id)
+  const character = await Character.findById(id);
 
-  if(!character){
-    return res.status(404).send({message: "Esse persomagem não existe!"})
+  if (!character) {
+    return res.status(404).send({ message: "Esse persomagem não existe!" });
   }
 
-  await character.remove()
- 
+  await character.remove();
+
   res.send({ message: "Persongaem remove com sucesso com sucesso" });
 });
 
